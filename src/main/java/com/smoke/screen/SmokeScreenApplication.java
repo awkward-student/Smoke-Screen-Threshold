a89fb06705d6fed42efaa6cb47f426111427753d
Vishwas <im.vishwas.03@gmail.com>
2023-06-04 16:37:34 +0530

    Initialize code-ground

---
+++ src/main/java/com/smoke/screen/SmokeScreenApplication.java
@@ -0,0 +1,13 @@
+package com.smoke.screen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeScreenApplication.class, args);
+	}
+
+}
